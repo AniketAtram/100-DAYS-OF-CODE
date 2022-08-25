776ec52ccbbb232ca99ecfcc0c592a1e94d1cd64
AniketAtram <anniketatram@gmail.com>
2022-08-25 18:51:58 +0530

    finished day-1

---
+++ Day-1/day_1.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n").title()
+animal = input("What is the name of your pet animal?\n").title()
+print("Your band name is " + city + "'s" + " " + animal)
