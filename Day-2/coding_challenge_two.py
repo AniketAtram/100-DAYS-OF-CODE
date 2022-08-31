a43901462a43e0a44bc2eeeabd7ae2c1c340716b
AniketAtram <anniketatram@gmail.com>
2022-08-31 12:47:23 +0530

    finished coding challenges

---
+++ Day-2/coding_challenge_two.py
@@ -0,0 +1,6 @@
+current_age = int(input("What is you current age? "))
+remaining_years = 90 - current_age
+number_of_days_remaining = 365 * remaining_years
+number_of_weeks_remaining = 52 * remaining_years
+number_of_months_remaining = 12 * remaining_years
+print(f"You have {number_of_days_remaining} days {number_of_weeks_remaining} weeks, and  {number_of_months_remaining} months left.")
